ed5bb4a3882e653ea8adfaceb80843347673ebaf
Haizar Yusuf Fahrezzy <haizaryusuf@gmail.com>
2025-11-20 15:34:15 +0700

    menambahkan file app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 pages = [
-    st.Page(page="pages/page01.py", tittle="Home", icon="🏡"),
-    st.Page(page="pages/page02.py", tittle="Visualisasi Data", icon= "📊"),
-    st.Page(page="pages/page03.py", tittle="Settings", icon= "⚙"),
+    st.Page(page="pages/pages01.py", tittle="Home", icon="🏡"),
+    st.Page(page="pages/pages02.py", tittle="Visualisasi Data", icon= "📊"),
+    st.Page(page="pages/pages03.py", tittle="Settings", icon= "⚙"),
 ]
 
 pg = st.navigation(
